56d28a319de4ae5ceef1e026f18de94cbeee7858
yeeydu <yeeysonduarte@gmail.com>
2023-08-09 11:24:29 +0100

    add crypto api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+export type Crypto = {
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  circulating_supply: number;
+  current_price: number;
+  fully_diluted_valuation: number;
+  high_24h: number;
+  id: string;
+  image: string;
+  last_updated: string;
+  low_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  market_cap_rank: number;
+  max_supply: number;
+  name: string;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  roi: null;
+  symbol: string;
+  total_supply: number;
+  total_volume: number;
+};
 
 function App() {
+  //                               Crypto[] Crypto type and Array or null
+  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+
+  useEffect(() => {
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+    axios.get(url).then((response) => {
+      setCryptos(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cryptos
+        ? cryptos.map((crypto) => {
+            return <p>{crypto.name}</p>;
+          })
+        : null}
     </div>
   );
 }
