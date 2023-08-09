1460b7a965fc2edb50b0eaead200651f60aa5f00
yeeydu <yeeysonduarte@gmail.com>
2023-08-09 15:17:37 +0100

    add chart for crypto values

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,48 @@ import "./App.css";
 import axios from "axios";
 import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./components/Types";
+import type { ChartData, ChartOptions } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function App() {
   //                               Crypto[] Crypto type and Array or null
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  // charts variables
+  const [data, setData] = useState<ChartData<"line">>();
+  const [options, setOptions] = useState<ChartOptions<"line">>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  });
 
   useEffect(() => {
     const url =
@@ -22,8 +59,39 @@ function App() {
       <div className="App">
         <select
           onChange={(e) => {
-            const c = cryptos?.find((x) => x.id === e.target.value);
-            setSelected(c);
+            const coin = cryptos?.find((x) => x.id === e.target.value);
+            setSelected(coin);
+            //request
+            axios
+              .get(
+                `https://api.coingecko.com/api/v3/coins/${coin?.id}/market_chart?vs_currency=eur&days=30&interval=daily`
+              )
+              .then((response) => {
+                setData({
+                  // DATE values
+                  labels: response.data.prices.map((price: number[]) => {
+                    //divide value by 1000 because of miliseconds
+                    return moment.unix(price[0] / 1000).format("MM-DD");
+                  }),
+                  datasets: [
+                    {
+                      label: "Dataset 1",
+                      data: response.data.prices.map((price: number[]) => {
+                        return price[1];
+                      }),
+                      borderColor: "rgb(255, 99, 132)",
+                      backgroundColor: "rgba(255, 99, 132, 0.5)",
+                    },
+                    // {
+                    //   label: 'Dataset 2',
+                    //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+                    //   borderColor: 'rgb(53, 162, 235)',
+                    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                    // },
+                  ],
+                });
+              });
+            //update data state
           }}
           defaultValue="default"
         >
@@ -40,6 +108,12 @@ function App() {
         </select>
       </div>
       {selected ? <CryptoSummary crypto={selected} /> : null}
+      {/*render a line chart*/}
+      {data ? (
+        <div style={{ width: 600 }}>
+          <Line options={options} data={data} />
+        </div>
+      ) : null}
     </>
   );
 }
