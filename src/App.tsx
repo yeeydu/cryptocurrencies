bd881d02a8f84725aec4cbffb3b814bc1795f47d
yeeydu <yeeysonduarte@gmail.com>
2023-08-10 11:02:05 +0100

    add dropdown range time, add title

--- src/App.tsx
+++ src/App.tsx
@@ -31,13 +31,15 @@ function App() {
   //                               Crypto[] Crypto type and Array or null
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  const [range, setRange] = useState<string>("30");
   // charts variables
   const [data, setData] = useState<ChartData<"line">>();
   const [options, setOptions] = useState<ChartOptions<"line">>({
     responsive: true,
     plugins: {
       legend: {
-        position: "top" as const,
+        // position: "top" as const,
+        display: false,
       },
       title: {
         display: true,
@@ -46,6 +48,7 @@ function App() {
     },
   });
 
+  // API GET ALl cryptos
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
@@ -54,6 +57,57 @@ function App() {
     });
   }, []);
 
+  // API Get selected crypto and range // if range is 1 day set intervals in hours
+  useEffect(() => {
+    if(!selected) return; // avoid error at load because nothing is selected
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=eur&days=${range}&interval=daily` //${range === '1' ? 'interval=hourly':'interval=daily'} hourly interval only enterprise plan
+      )
+      .then((response) => {
+        setData({
+          // DATE values
+          labels: response.data.prices.map((price: number[]) => {
+            //divide value by 1000 because of miliseconds
+            return moment
+              .unix(price[0] / 1000)
+              .format(range === "1" ? "HH:MM" : "MM-DD");
+          }),
+          datasets: [
+            {
+              label: "Dataset 1",
+              data: response.data.prices.map((price: number[]) => {
+                return price[1];
+              }),
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            // {
+            //   label: 'Dataset 2',
+            //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            //   borderColor: 'rgb(53, 162, 235)',
+            //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            // },
+          ],
+        }); // set
+        setOptions({
+          responsive: true,
+          plugins: {
+            legend: {
+              //position: "top" as const,
+              display: false,
+            },
+            title: {
+              display: true,
+              text:
+                `${selected?.name} Price over last ${range}` +
+                (range === "1" ? " day" : " days."),
+            },
+          },
+        });
+      });
+  }, [selected, range]);
+
   return (
     <>
       <div className="App">
@@ -61,37 +115,6 @@ function App() {
           onChange={(e) => {
             const coin = cryptos?.find((x) => x.id === e.target.value);
             setSelected(coin);
-            //request
-            axios
-              .get(
-                `https://api.coingecko.com/api/v3/coins/${coin?.id}/market_chart?vs_currency=eur&days=30&interval=daily`
-              )
-              .then((response) => {
-                setData({
-                  // DATE values
-                  labels: response.data.prices.map((price: number[]) => {
-                    //divide value by 1000 because of miliseconds
-                    return moment.unix(price[0] / 1000).format("MM-DD");
-                  }),
-                  datasets: [
-                    {
-                      label: "Dataset 1",
-                      data: response.data.prices.map((price: number[]) => {
-                        return price[1];
-                      }),
-                      borderColor: "rgb(255, 99, 132)",
-                      backgroundColor: "rgba(255, 99, 132, 0.5)",
-                    },
-                    // {
-                    //   label: 'Dataset 2',
-                    //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-                    //   borderColor: 'rgb(53, 162, 235)',
-                    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-                    // },
-                  ],
-                });
-              });
-            //update data state
           }}
           defaultValue="default"
         >
@@ -105,6 +128,16 @@ function App() {
                 );
               })
             : null}
+        </select>
+        <select
+          onChange={(e) => {
+            setRange(e.target.value);
+            //parseInt(setRange(e.target.value)); if days where numbers
+          }}
+        >
+          <option value="30">30 days</option>
+          <option value="7">7 days</option>
+          <option value="1">1 day</option>
         </select>
       </div>
       {selected ? <CryptoSummary crypto={selected} /> : null}
